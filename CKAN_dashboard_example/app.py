bdbcbc4184f93d99f3921b56eeff1b2ce026da11
yanique <yanique.campbell@umanitoba.ca>
2025-09-10 08:19:07 -0500

    Update app.py

--- CKAN_dashboard_example/app.py
+++ CKAN_dashboard_example/app.py
@@ -14,8 +14,8 @@ st.set_page_config(page_title="James Bay CTD Dashboard", layout="wide")
 # Refresh every 600 seconds (12 minutes)
 #st_autorefresh(interval=720 * 1000, limit=100, key="datarefresh")
 
-
-t1, t2, t3, t4 =st.tabs(["Introduction","Station Map", "Depth Profiles", " Depth Profile Over Time"])
+st.sidebar.markdown("### 📂 **Select View**")
+page = st.sidebar.radio("Select", ["Introduction", "Latest Data", "Station Map", "Depth Profiles", "Animated Profiles"])
 
 if st.button("Refresh Data"):
     st.experimental_rerun()
@@ -55,7 +55,8 @@ df.dropna(subset=["Depth", "Temperature", "Salinity", "Conductivity", "Latitude"
 #df["Timestamp"] = pd.to_datetime(df["Timestamp"])
 
 # Sidebar filters
-st.sidebar.header("🔍 Filter Data")
+st.sidebar.markdown(" ")
+st.sidebar.markdown("### 🔍 **Filter Data**")
 
 stations = df["Station"].unique()
 selected_station = st.sidebar.selectbox("Select Station", stations)
@@ -73,80 +74,83 @@ min_time = df["Timestamp"].min()
 max_time = df["Timestamp"].max()
 
 # Use pandas Timestamps in the slider
-selected_time = st.sidebar.slider(
-    "Select Time Range",
-    min_value=min_time,
-    max_value=max_time,
-    value=(min_time.to_pydatetime(), max_time.to_pydatetime()),
-    format="YYYY-MM-DD HH:mm"
-)
+# selected_time = st.sidebar.slider(
+#     "Select Time Range",
+#     min_value=min_time,
+#     max_value=max_time,
+#     value=(min_time.to_pydatetime(), max_time.to_pydatetime()),
+#     format="YYYY-MM-DD HH:mm"
+# )
 
-selected_time_utc = (
-    selected_time[0].replace(tzinfo=datetime.timezone.utc),
-    selected_time[1].replace(tzinfo=datetime.timezone.utc)
-)
+# selected_time_utc = (
+#     selected_time[0].replace(tzinfo=datetime.timezone.utc),
+#     selected_time[1].replace(tzinfo=datetime.timezone.utc)
+# )
 
 # Filtered data
 filtered_df = df[
     (df["Station"] == selected_station) &
     (df["Depth"] >= depth_range[0]) &
-    (df["Depth"] <= depth_range[1]) &
-    (df["Timestamp"] >= selected_time_utc[0]) &
-    (df["Timestamp"] <= selected_time_utc[1])
+    (df["Depth"] <= depth_range[1]) 
+    # (df["Timestamp"] >= selected_time_utc[0]) &
+    # (df["Timestamp"] <= selected_time_utc[1])
 ]
 
+st.sidebar.markdown("---")
+st.sidebar.caption("Made with ❤️ by Yan")
 
-with t1:
 
-
+if page == "Introduction":
     # Title and intro
     st.title("🌊 James Bay CTD 2022 Dashboard")
-    st.markdown("""
-    This dashboard visualizes oceanographic measurements collected using a CTD (Conductivity, Temperature, Depth) instrument in James Bay, 2022.
+    st.markdown("This dashboard visualizes oceanographic measurements collected using a CTD (Conductivity, Temperature, Depth) instrument in James Bay, 2022.")
 
-    A CTD is a key tool in oceanography that measures how water properties change with depth. Temperature and salinity influence water density, which drives ocean circulation and affects marine ecosystems.
-    """)
+    with st.expander("🔬 More About CTDs"):
 
-    # # Interpretation
-    # st.subheader("🔬 More about CTDs")
-    # st.markdown("""
-    # The CTD instrument provides a vertical profile of the water column:
+        st.markdown("""
+            A CTD is a key tool in oceanography that measures how water properties change with depth. Temperature and salinity influence water density, which drives ocean circulation and affects marine ecosystems.
+            
+            The CTD provides a vertical profile of the water column:
+            - **Temperature** typically decreases with depth, indicating thermal stratification.
+            - **Salinity** can vary due to freshwater input from rivers or ice melt, especially near the surface.
+            - **Conductivity** reflects salinity and temperature, helping infer water mass characteristics.
 
-    # - **Temperature** typically decreases with depth, indicating thermal stratification.
-    # - **Salinity** can vary due to freshwater input from rivers or ice melt, especially near the surface.
-    # - **Conductivity** reflects salinity and temperature, helping infer water mass characteristics.
+            In James Bay, surface waters may be fresher due to river discharge, while deeper layers are colder and more saline. This stratification influences nutrient mixing, biological productivity, and carbon cycling.
 
-    # In James Bay, surface waters may be fresher due to river discharge, while deeper layers are colder and more saline. This stratification influences nutrient mixing, biological productivity, and carbon cycling.
-
-    # By comparing stations over time, we can detect seasonal changes, freshwater plumes, or mixing zones—critical for understanding climate impacts in sub-Arctic marine environments.
-    # """)
+            By comparing stations over time, we can detect seasonal changes, freshwater plumes, or mixing zones—critical for understanding climate impacts in sub-Arctic marine environments.
+            """)
 
     # Footer
-    st.markdown("📁 [Dataset Source - CanWIN](https://canwin-datahub.ad.umanitoba.ca/data/dataset/james-bay-ctd-2022)")
-    st.markdown("---")
+    st.markdown("""
+        <a href="https://canwin-datahub.ad.umanitoba.ca/data/dataset/james-bay-ctd-2022" target="_blank">
+            <button style='background-color: #007ACC; color: white; padding: 0.5em 1em; border: none; border-radius: 5px; cursor: pointer;'>
+                📁 View Data on CanWIN
+            </button>
+        </a>
+    """, unsafe_allow_html=True)
+    st.markdown("####")
 
 
-    st.subheader("📊 Latest Data From Github")
-    st.markdown('Data is being updated every 12 minutes')
+if page == "Latest Data":
+    with st.container(border=True):
+        st.subheader("📊 Latest Data From Github")
+        csv_path = "2022_ctd.csv"
+        # Get UTC modified time
+        utc_time = datetime.datetime.fromtimestamp(os.path.getmtime(csv_path), tz=datetime.timezone.utc)
 
-    csv_path = "2022_ctd.csv"
-    # Get UTC modified time
-    utc_time = datetime.datetime.fromtimestamp(os.path.getmtime(csv_path), tz=datetime.timezone.utc)
+        from zoneinfo import ZoneInfo
+        # Convert to local time (Winnipeg)
+        local_time = utc_time.astimezone(ZoneInfo("America/Winnipeg"))
+        next_update = local_time + datetime.timedelta(minutes=10)
 
-    from zoneinfo import ZoneInfo
-    # Convert to local time (Winnipeg)
-    local_time = utc_time.astimezone(ZoneInfo("America/Winnipeg"))
-    next_update = local_time + datetime.timedelta(minutes=10)
+        st.markdown("##### ⏱️ Update Info")
+        st.write(f"**Last updated:** {local_time.strftime('%Y-%m-%d %H:%M:%S')}")
+        st.write(f"**Next update expected:** {next_update.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    st.markdown("##### ⏱️ Update Info")
-    st.write(f"**Last updated:** {local_time.strftime('%Y-%m-%d %H:%M:%S')}")
-    st.write(f"**Next update expected:** {next_update.strftime('%Y-%m-%d %H:%M:%S')}")
-
-    st. dataframe(df_from_github)
+        st. dataframe(df_from_github)
 
 
-
-with t2:
+if page == "Station Map":
     st.subheader("📍 CTD Station Map")
 
     # Spinner while loading
@@ -169,8 +173,6 @@ with t2:
         map_df["Station"] = map_df["Station"].astype(str)
         map_df["TimestampStr"] = map_df["TimestampStr"].astype(str)
         map_df["Color"] = map_df["Color"].apply(lambda x: list(map(int, x)))
-
-
 
         # Create PyDeck layer
         layer = pdk.Layer(
@@ -205,8 +207,7 @@ with t2:
         ))
 
 
-
-with t3:
+if page == "Depth Profiles":
     # Depth profiles
     st.subheader("📊 Interactive Depth Profiles")
     st.caption(f"Available data from {df['Timestamp'].min().strftime('%Y-%m-%d')} to {df['Timestamp'].max().strftime('%Y-%m-%d')}")
@@ -222,11 +223,11 @@ with t3:
             y="Depth",
             color="Station",
             hover_data=["Timestamp", "Salinity", "Conductivity"],
-            title="Temperature vs Depth",
+            title="Temperature vs Depth 🔥",
             labels={"Depth": "Depth (m)", "Temperature": "Temperature (°C)"}
         )
         fig_temp.update_yaxes(autorange="reversed")
-        st.plotly_chart(fig_temp, use_container_width=True)
+        #st.plotly_chart(fig_temp, use_container_width=True)
 
         # Salinity vs Depth
         fig_sal = px.scatter(
@@ -235,11 +236,11 @@ with t3:
             y="Depth",
             color="Station",
             hover_data=["Timestamp", "Temperature", "Conductivity"],
-            title="Salinity vs Depth",
+            title="Salinity vs Depth 🧂",
             labels={"Depth": "Depth (m)", "Salinity": "Salinity (PSU)"}
         )
         fig_sal.update_yaxes(autorange="reversed")
-        st.plotly_chart(fig_sal, use_container_width=True)
+        #st.plotly_chart(fig_sal, use_container_width=True)
 
         # Conductivity vs Depth
         fig_cond = px.scatter(
@@ -248,14 +249,28 @@ with t3:
             y="Depth",
             color="Station",
             hover_data=["Timestamp", "Temperature", "Salinity"],
-            title="Conductivity vs Depth",
+            title="Conductivity vs Depth ⚡️",
             labels={"Depth": "Depth (m)", "Conductivity": "Conductivity (mS/cm)"}
         )
         fig_cond.update_yaxes(autorange="reversed")
-        st.plotly_chart(fig_cond, use_container_width=True)
+        #st.plotly_chart(fig_cond, use_container_width=True)
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            with st.container(border=True):
+                st.metric("Avg Temp", f"{filtered_df['Temperature'].mean():.2f} °C")
+                st.plotly_chart(fig_temp, use_container_width=True)
+        with col2:
+            with st.container(border=True):
+                st.metric("Max Salinity", f"{filtered_df['Salinity'].max():.2f} PSU")
+                st.plotly_chart(fig_sal, use_container_width=True)
+        with col3:
+            with st.container(border=True):
+                st.metric("Conductivity", f"{filtered_df['Conductivity'].max():.2f}")
+                st.plotly_chart(fig_cond, use_container_width=True)
 
 
-with t4:
+if page == "Animated Profiles":
     import plotly.express as px
     st.subheader("🎞️ Animated Depth Profile Over Time")
 

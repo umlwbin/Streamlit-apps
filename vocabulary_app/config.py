08db7b2e4d3551157378c735b8747c45c4f8e5fb
yanique <yanique.campbell@umanitoba.ca>
2026-05-04 10:12:27 -0500

    Sync 'vocabulary-app' → 'vocabulary_app' from GitLab

--- vocabulary_app/config.py
+++ vocabulary_app/config.py
@@ -1,4 +1,5 @@
-SHEET_ID = "1jodEHgJm6uEpanvpnUqVAFutbWBgGfrISwAyDaaGHI0"
-SHEET_URL = f"https://docs.google.com/spreadsheet/ccc?key={SHEET_ID}&output=csv"
+SHEET_ID = "2PACX-1vTvaZKIEBZxDlSVSCw3qctqNTKdsC4fQbnjQPC2RfPhelqnud9jGKGN37tkXkmLaccIvisPYPxMgpqv"
+#SHEET_URL = f"https://docs.google.com/spreadsheet/ccc?key={SHEET_ID}&output=csv"
 
+SHEET_URL= f"https://docs.google.com/spreadsheets/d/e/{SHEET_ID}/pub?output=csv"
 LOGO_PATH = ("img/UM-EarthObservationScience-cmyk-left.png")
